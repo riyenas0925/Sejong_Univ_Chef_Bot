a4490e739f2c1b8e031da53da1097531e1ad1a75
2kyung19 <32586712+2kyung19@users.noreply.github.com>
2018-05-02 20:35:45 +0900

    파싱 키(인덱스)

--- parsing.py
+++ parsing.py
@@ -10,6 +10,7 @@ soup = BeautifulSoup(text, 'html.parser')
 menu = soup.find_all('div',{'class':'th'})
 price = soup.find_all('div',{'class':'td price'})
 
+##리스트 menu와 price에 쓰레기값 제거
 for n in menu:
     i = menu.index(n)
     menu[i]= n.get_text()
@@ -17,8 +18,15 @@ for n in price:
     i = price.index(n)
     price[i]= n.get_text()
 
-foodlist=[]
+##딕셔너리 생성
+message={}
+foodlist={}
 
-for i in range(0,len(menu)-1):
-    foodlist.append((menu[i],price[i]))
+for i in range(0,len(menu)):
+    foodlist[i+1]=menu[i]+" "+price[i]
 
+message["message"]=foodlist
+
+print(json.dumps(message,indent=2))
+
+##자꾸 랜덤출력해서 인덱스를 key로 넣음(딕셔너리)
