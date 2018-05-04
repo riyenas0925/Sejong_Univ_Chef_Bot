eb346fd2613dbb6372dba717d8a45a544e16e0ca
2kyung19 <32586712+2kyung19@users.noreply.github.com>
2018-05-05 04:49:40 +0900

    get_text왜안될까

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ fri = soup.find_all('tr',{'class':'seq-05'})
 sat = soup.find_all('tr',{'class':'seq-06'})
 
 day = [mon,tue,wed,thu,fri,sat]
+daylist = [1,2,3,4,5]
 food = []
 
 for i in day:
@@ -27,7 +28,17 @@ for i in day:
 
 for i in food:
     j = food.index(i)
-    food[j]=i.get_text()
+    
+
+for i in range(0,5):
+    print (daylist[i])
+    print ("-------------")
+    print ("<프리미엄>\n",food[5*i])
+    print ("<일품>\n",food[5*i+1])
+    print ("<양식>\n",food[5*i+2])
+    print ("<한식>\n",food[5*i+3])
+    print ("<분식>\n",food[5*i+4])
+    
 
 #food
 #0~4 월 5~9 화 10~14 수 15~19 목 20~24 금 25~29 토
