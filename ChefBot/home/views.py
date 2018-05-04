0deaf06d05a45f5a622305a48a9c926847217492
riyenas0925 <riyenas0925@gmail.com>
2018-05-05 05:02:42 +0900

    군자관 파싱 장고 추가

--- ChefBot/home/views.py
+++ ChefBot/home/views.py
@@ -29,7 +29,7 @@ def message(request):
             return repeat+h_menu()
 
         elif place=='군자관':
-            return repeat+"(ง˙∇˙)ว\n아직 준비 중..."
+            return repeat+g_menu()
             #군자 파싱 함수 만들면 뒤에 이어주면 됨
 
         elif place=='우정당':
@@ -87,5 +87,46 @@ def h_menu():
 
     for i in range(0,len(menu)):
         foodlist += menu[i] + "  " + price[i] + "\n"
+
+    return foodlist
+
+def_g_menu():
+
+    req = urllib.request.Request("http://m.sejong.ac.kr/front/cafeteria.do?type1=3", headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(req)
+    text = response.read().decode("utf8")
+
+    soup = BeautifulSoup(text, 'html.parser')
+
+    day = soup.find_all("th",{'rowspan':'2'})
+    menu = soup.find_all('div',{'class':'td'})
+
+    def time(k):
+        if k % 2 == 0:
+            return '중식'
+
+        else:
+            return '석식'
+
+    ##리스트 menu와 price에 쓰레기값 제거
+    for n in day:
+        i = day.index(n)
+        day[i]= n.get_text().replace("\n","")
+
+
+    for n in menu:
+        i = menu.index(n)
+        menu[i]= n.get_text().replace("\t","").replace("\r","").replace("\n\n\n"," ").replace("\n\n"," ").replace("\n"," ").replace(" ","\n").replace("\n\n\n\n\n\n\n\n\n\n\n\n\n\n","")
+
+    foodlist=""
+    cnt = 0
+
+    for i in range(0,12):
+        if i % 2 == 0:
+            foodlist += '\n-----------\n\n' + day[cnt] + '\n\n' + time(i) + menu[i] + '\n'
+            cnt += 1
+
+        else:
+            foodlist += time(i) + menu[i]
 
     return foodlist
