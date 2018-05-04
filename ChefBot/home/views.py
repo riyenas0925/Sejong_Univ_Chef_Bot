3a5f96595e3e55858b18647fba82a70c2b55049d
riyenas0925 <riyenas0925@gmail.com>
2018-05-04 12:42:19 +0900

    aws 서버 이전

--- ChefBot/home/views.py
+++ ChefBot/home/views.py
@@ -4,11 +4,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from bs4 import BeautifulSoup
 import urllib.request
-import datetime
 import json
+import datetime
 
 
-# Create your views here.
 def keyboard(request):
     return JsonResponse(
         {
@@ -26,7 +25,7 @@ def message(request):
 
     return JsonResponse({ #return 밑에는 공통어
         "message": {
-            "text": return_date + return_str + '메뉴입니다. \n\n'
+            "text": return_date + return_str + '메뉴입니다. \n\n' + h_menu()
         },
         "keyboard": {
             "type" : "buttons",
@@ -34,19 +33,24 @@ def message(request):
         }
     })
 
+
 def h_menu():
-    html = urllib.request.urlopen("http://m.sejong.ac.kr/front/cafeteria.do")
-    text = html.read().decode("utf8")
+
+    req = urllib.request.Request("http://m.sejong.ac.kr/front/cafeteria.do", headers={'User-Agent': 'Mozilla/5.0'})
+    con = urllib.request.urlopen(req)
+    text = con.read().decode("utf8")
 
     soup = BeautifulSoup(text, 'html.parser')
 
     menu = soup.find_all('div',{'class':'th'})
     price = soup.find_all('div',{'class':'td price'})
 
+
     ##리스트 menu와 price에 쓰레기값 제거
     for n in menu:
         i = menu.index(n)
         menu[i]= n.get_text()
+
     for n in price:
         i = price.index(n)
         price[i]= n.get_text()
---
+++ test.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+return_str = '우정당'
+
+req = urllib.request.Request("http://m.sejong.ac.kr/front/cafeteria.do", headers={'User-Agent': 'Mozilla/5.0'})
+response = urllib.request.urlopen(req)
+text = response.read().decode("utf8")
+
+'''
+html = urllib.request.urlopen("http://m.sejong.ac.kr/front/cafeteria.do")
+text = html.read().decode("utf8")
+'''
+soup = BeautifulSoup(text, 'html.parser')
+
+menu = soup.find_all('div',{'class':'th'})
+price = soup.find_all('div',{'class':'td price'})
+##리스트 menu와 price에 쓰레기값 제거
+for n in menu:
+    i = menu.index(n)
+    menu[i]= n.get_text()
+for n in price:
+    i = price.index(n)
+    price[i]= n.get_text()
+
+foodlist=""
+
+for i in range(0,len(menu)):
+    foodlist += menu[i] +""+price[i]+"\n"
+
+print(foodlist)
+print(urllib.urlopen(url).getcode())
