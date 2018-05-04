595aacb3230bfb9a04fbfaef5b5c782b5f338959
riyenas0925 <riyenas0925@gmail.com>
2018-05-04 23:44:27 +0900

    delete, friend 삭제

--- ChefBot/ChefBot/urls.py
+++ ChefBot/ChefBot/urls.py
@@ -21,6 +21,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^keyboard', views.keyboard),
     url(r'^message', views.message),
-    url(r'^delete', views.delete),
-    url(r'^friend', views.friend),
 ]
