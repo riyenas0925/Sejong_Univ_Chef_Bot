0bdbe777d02f9e16a481449881531639f25e912e
riyenas0925 <riyenas0925@gmail.com>
2018-05-05 20:17:53 +0900

    군자관 요일 선택

--- ChefBot/home/views.py
+++ ChefBot/home/views.py
@@ -30,7 +30,7 @@ def message(request):
             return repeat+h_menu()
 
         elif place=='군자관':
-            return repeat+g_menu()
+            return repeat+g_menu(return_str)
             #군자 파싱 함수 만들면 뒤에 이어주면 됨
 
         elif place=='우정당':
@@ -93,12 +93,11 @@ def h_menu():
 
     return foodlist
 
-def g_menu():
-
+def g_menu(type):
     req = urllib.request.Request("http://m.sejong.ac.kr/front/cafeteria.do?type1=3", headers={'User-Agent': 'Mozilla/5.0'})
     response = urllib.request.urlopen(req)
     text = response.read().decode("utf8")
- 
+
     soup = BeautifulSoup(text, 'html.parser')
 
     day = soup.find_all("th",{'rowspan':'2'})
@@ -121,18 +120,41 @@ def g_menu():
         i = menu.index(n)
         menu[i]= n.get_text().replace("\t","").replace("\r","").replace("\n\n\n"," ").replace("\n\n"," ").replace("\n"," ").replace(" ","\n").replace("\n\n\n\n\n\n\n\n\n\n\n\n\n\n","")
 
-    foodlist=""
+    temp=""
+    foodlist_day=['','','','','','']
+    foodlist_all=''
     cnt = 0
 
     for i in range(0,12):
         if i % 2 == 0:
-            foodlist += day[cnt] + '\n\n' + time(i) + menu[i]
+            foodlist_day[cnt-1] = temp
+            temp=""
+
+            temp += day[cnt] + '\n\n' + time(i) + menu[i]
             cnt += 1
 
         else:
-            foodlist += '\n' + time(i) + menu[i] + '\n---------------\n\n'
-    
-    return foodlist
+            temp += '\n' + time(i) + menu[i] + '\n---------------\n\n'
+
+    foodlist_all = foodlist_day[0] + foodlist_day[1] + foodlist_day[2] + foodlist_day[3] + foodlist_day[4]
+
+    if type == '군자관 월':
+        return foodlist_day[0]
+
+    elif type == '군자관 화':
+        return foodlist_day[1]
+
+    elif type == '군자관 수':
+        return foodlist_day[2]
+
+    elif type == '군자관 목':
+        return foodlist_day[3]
+
+    elif type == '군자관 금':
+        return foodlist_day[4]
+
+    else:
+        return foodlist_all
     
 def u_menu():
     message = ((request.body).decode('utf-8'))
--- Test_file/gun_menu.py
+++ Test_file/gun_menu.py
@@ -1,41 +1,68 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-req = urllib.request.Request("http://m.sejong.ac.kr/front/cafeteria.do?type1=3", headers={'User-Agent': 'Mozilla/5.0'})
-response = urllib.request.urlopen(req)
-text = response.read().decode("utf8")
+def g_menu(type):
+    req = urllib.request.Request("http://m.sejong.ac.kr/front/cafeteria.do?type1=3", headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(req)
+    text = response.read().decode("utf8")
 
-soup = BeautifulSoup(text, 'html.parser')
+    soup = BeautifulSoup(text, 'html.parser')
 
-day = soup.find_all("th",{'rowspan':'2'})
-menu = soup.find_all('div',{'class':'td'})
+    day = soup.find_all("th",{'rowspan':'2'})
+    menu = soup.find_all('div',{'class':'td'})
 
-def time(k):
-    if k % 2 == 0:
-        return '<중식>'
+    def time(k):
+        if k % 2 == 0:
+            return '<중식>'
+
+        else:
+            return '<석식>'
+
+    ##리스트 menu와 price에 쓰레기값 제거
+    for n in day:
+        i = day.index(n)
+        day[i]= n.get_text().replace("\n","")
+
+
+    for n in menu:
+        i = menu.index(n)
+        menu[i]= n.get_text().replace("\t","").replace("\r","").replace("\n\n\n"," ").replace("\n\n"," ").replace("\n"," ").replace(" ","\n").replace("\n\n\n\n\n\n\n\n\n\n\n\n\n\n","")
+
+    temp=""
+    foodlist_day=['','','','','','']
+    foodlist_all=''
+    cnt = 0
+
+    for i in range(0,12):
+        if i % 2 == 0:
+            foodlist_day[cnt-1] = temp
+            temp=""
+
+            temp += day[cnt] + '\n\n' + time(i) + menu[i]
+            cnt += 1
+
+        else:
+            temp += '\n' + time(i) + menu[i] + '\n---------------\n\n'
+
+    foodlist_all = foodlist_day[0] + foodlist_day[1] + foodlist_day[2] + foodlist_day[3] + foodlist_day[4]
+
+    if type == '군자관 월':
+        return foodlist_day[0]
+
+    elif type == '군자관 화':
+        return foodlist_day[1]
+
+    elif type == '군자관 수':
+        return foodlist_day[2]
+
+    elif type == '군자관 목':
+        return foodlist_day[3]
+
+    elif type == '군자관 금':
+        return foodlist_day[4]
 
     else:
-        return '<석식>'
-
-##리스트 menu와 price에 쓰레기값 제거
-for n in day:
-    i = day.index(n)
-    day[i]= n.get_text().replace("\n","")
+        return foodlist_all
 
 
-for n in menu:
-    i = menu.index(n)
-    menu[i]= n.get_text().replace("\t","").replace("\r","").replace("\n\n\n"," ").replace("\n\n"," ").replace("\n"," ").replace(" ","\n").replace("\n\n\n\n\n\n\n\n\n\n\n\n\n\n","")
-
-foodlist=""
-cnt = 0
-
-for i in range(0,12):
-    if i % 2 == 0:
-        foodlist += day[cnt] + '\n\n' + time(i) + menu[i]
-        cnt += 1
-
-    else:
-        foodlist += '\n' + time(i) + menu[i] + '\n---------------\n\n'
-
-print(foodlist)
+print(g_menu("군자관"))
