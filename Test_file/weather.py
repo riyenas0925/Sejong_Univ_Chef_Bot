5be506927ec45aa497744ded0cd22d2436927753
riyenas0925 <riyenas0925@gmail.com>
2018-07-10 16:51:57 +0900

    test

--- Test_file/weather.py
+++ Test_file/weather.py
@@ -5,7 +5,7 @@ def weather():
     params = {"version": "1", "city":"서울", "county":"광진구","village":"군자동"}
     headers = {"appKey": "ea6c12af-2d3c-4572-a9a9-30d42a9742d4"}
     response = requests.get("https://api2.sktelecom.com/weather/current/minutely", params=params, headers=headers)
-
+    
     data = json.loads(response.text)
 
     weather = data["weather"]["minutely"]
